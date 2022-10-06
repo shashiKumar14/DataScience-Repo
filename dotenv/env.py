751fbd9ea1b5dd02ce33546c0726419b82e9d3a5
EGA SHASHI KUMAR <47054559+shashiKumar14@users.noreply.github.com>
2022-10-06 09:45:03 +0530

    Create env.py

---
+++ dotenv/env.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+#we have to use USERNAME , PASSWORD variable in the code
+USERNAME = os.environ['USER_NAME']
+PASSWORD = os.environ['PASS_WORD']
+
+#In .env file
+USER_NAME = "username"
+PASS_WORD = "password"
