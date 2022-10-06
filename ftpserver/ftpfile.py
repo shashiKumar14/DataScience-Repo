97c614775c54c033f540e0ee2524225e8ffdfd57
EGA SHASHI KUMAR <47054559+shashiKumar14@users.noreply.github.com>
2022-10-06 10:21:44 +0530

    Create ftpfile.py

---
+++ ftpserver/ftpfile.py
@@ -0,0 +1,22 @@
+import ftplib
+import wget
+import os
+
+#To connect with file server
+ftp_server = ftplib.FTP(HOSTNAME, USERNAME, PASSWORD)
+ftp_server.encoding ='utf-8'
+
+ftp_server.cwd("/path/to/folder")
+list_of_files=ftp_server.nlst()#we can loop the list_of_files for our use
+
+#To use we have to download first
+for file in list_of_files:
+  link = f'ftp://{USERNAME}:{PASSWORD}@{HOSTNAME}/path/to/folder/{file}'
+  page=wget.download(link)
+  #we can use this "page" now
+  #call function or write code
+  ftp_server.cwd("/path/to/folder")
+  ftp_server.delete(file)
+  os.remove(file)
+
+
