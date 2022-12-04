57d1f12be3bf962b66b3cb5f442d0095347e4a2a
EGA SHASHI KUMAR <47054559+shashiKumar14@users.noreply.github.com>
2022-12-04 22:11:02 +0530

    Create create venv and activate.py

---
+++ env-activate/create venv and activate.py
@@ -0,0 +1,15 @@
+python3 -m venv create_any_filename  ===example===>python3 -m venv .venv 
+ or 
+python -m venv create_any_filename  ===example===>python -m venv .venv 
+
+
+activate after creating 
+=========================:
+
+for wondows:
+============
+  create_any_filename\Scripts\activate.bat  
+  
+for ubuntu:
+===========
+  source create_any_filename/bin/activate  
